4c14f0ada721786dd99758baf3070d0f5c0eb837
Helen Her <her.helen@gmail.com>
2023-08-12 16:03:12 -0300

    colocando código do exercício feito em aula no pacote separado

---
+++ ada-poo-i/src/aula04/heranca/Moto.java
@@ -0,0 +1,16 @@
+package aula04.heranca;
+
+public class Moto extends Veiculo {
+    private int cilindradas;
+
+    public Moto(String placa, String cor, String modelo, int ano, int cilindradas) {
+        super(placa, cor, modelo, ano);
+        this.cilindradas = cilindradas;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Cilindradas: " + this.cilindradas);
+    }
+}
---
+++ ada-poo-i/src/aula04/heranca/TesteHeranca.java
@@ -0,0 +1,12 @@
+package aula04.heranca;
+
+public class TesteHeranca {
+    public static void main(String[] args) {
+        Carro carroGol = new Carro("AAA", "Cinza", "Gol", 2021, 4, "Flex");
+        carroGol.exibirDados();
+
+        Moto moto = new Moto("BBB", "Preto", "Kawasaki", 2023, 2000);
+        moto.exibirDados();
+
+    }
+}
